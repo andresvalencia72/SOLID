c7ee0e758b3881b2f753ef117f9c439c6ad98c1f
Andres Valencia <andresvalencia7270@gmail.com>
2023-05-29 18:18:51 +0200

    aplicando clean code a nombres de variables más expresivos

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,6 @@ const app = document.querySelector<HTMLDivElement>('#app')!
 app.innerHTML = `
   <h1>CleanCode y SOLID</h1>
   <span>Revisar la consola de JavaScript</span>
+  <p> Andrés Valencia 😁</p>
 `
 
